c95a049c5f6392e002e2467afe55741464be2476
Jesinug <jesica.nugnez@gmail.com>
2019-07-03 21:19:55 -0300

    Implementando Node como web server

---
+++ index.js
@@ -0,0 +1,47 @@
+//Aca empieza un basico createServer
+const http = require("http");
+const fs = require("fs");
+
+http.createServer((request, response) => {
+		
+		let dir = "public/"; //declaro la carpeta en donde debe ir a buscar archivos
+
+		let file = (request.url == "/") ? "index.html" : request.url;
+			file = (file.match(/[^.]+(\.[^?#]+)?/) || [])[0];
+		
+		let ext = file.substring( file.lastIndexOf(".") ).toLowerCase();
+
+		let types = {
+			".html"	: "text/html",
+			".js"	: "text/javascript",
+			".css"	: "text/css",
+			".txt" 	: "text/plain",
+			".json"	: "application/json",
+			".png"	: "image/png",
+			".jpg"	: "image/jpg",
+			".gif"	: "image/gif",
+			".ico"	: "image/x-icon",
+			".wav"	: "audio/wav",
+			".mp4"	: "video/mp4",
+			".woff"	: "application/font-woff",
+			".ttf"	: "application/font-ttf",
+			".eot"	: "application/vnd.ms-fontobject",
+			".otf"	: "application/font-otf",
+			".svg"	: "application/image/svg+xml"
+		};
+
+		let contentType = types[ext] || "application/octet-stream";
+
+		fs.readFile( dir + file, (error, content) => {
+			console.log("El recurso solicitado es: " + file)
+			if ( error ) {
+				response.writeHead(400, { "Content-Type" : "text/plain" } );
+				response.end("ARCHIVO NO ENCONTRADO");
+			} else {
+				response.writeHead(200, { "Content-Type" : contentType } );
+				response.end(content);
+			}
+
+		});
+
+}).listen(80);
